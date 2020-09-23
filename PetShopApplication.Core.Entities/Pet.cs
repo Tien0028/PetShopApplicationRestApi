0d5e62adca63dcec50dddeaea7233d69167d0079
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2020-09-23 14:58:13 +0200

    initial

---
+++ PetShopApplication.Core.Entities/Pet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PetShopApplication.Core.Entities
+{
+    public class Pet
+    {
+        public PetType PetType { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime Birthdate { get; set; }
+        public DateTime SoldDate { get; set; }
+        public string previousOwner { get; set; }
+        public Owner PetOwner { get; set; }
+        public double Price { get; set; }
+
+    }
+}
---
+++ PetShopApplication.Core/DomainService/IOwnerRepository.cs
@@ -0,0 +1,25 @@
+﻿using PetShopApplication.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShopApplication.Core.DomainService
+{
+    public interface IOwnerRepository
+    {
+
+        Owner Create(Owner owner);
+        List<Owner> ReadOwners();
+        Owner Update(Owner owner);
+        Owner DeleteOwner(int id);
+        Owner ReadOwnerById(int id);
+        IEnumerable<Owner> FindOwnerByName(string searchValue);
+        IEnumerable<Owner> FindOwnerByPhoneNummer(string searchValue);
+        IEnumerable<Owner> FindOwnerByAddress(string searchValue);
+        IEnumerable<Owner> FindOwnerByEmail(string searchValue);
+        List<Pet> FindAllPetsByOwner(Owner theOwner);
+        List<Owner> FindOwner(int ownerId);
+        IEnumerable <Owner> GetAllOwners();
+        List<Owner> FindOwnerById(int searchForOwner);
+    }
+}
